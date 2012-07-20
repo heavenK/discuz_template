11aef21cbd3bb563ddff33a6537d6ee9e44fbfe3
king <441203532@qq.com>
2012-07-20 10:44:37 +0800

    s

--- data/log/201207_illegallog.php
+++ data/log/201207_illegallog.php
@@ -4,3 +4,4 @@
 <?PHP exit;?>	1341391124	zhanglu	12***3	Ques #0	127.0.0.1
 <?PHP exit;?>	1341391447	lyhj0001	hea***00	Ques #0	127.0.0.1
 <?PHP exit;?>	1342579203	zhanglu	1***3	Ques #0	127.0.0.1
+<?PHP exit;?>	1342689267	admin	a***n	Ques #0	127.0.0.1
