44c992288ade0f41f4371fb05499b489cc5992fb
toamture <87068928@qq.com>
2012-07-27 10:37:14 +0800

    kk

--- source/module/forum/forum_forumdisplay.php
+++ source/module/forum/forum_forumdisplay.php
@@ -70,7 +70,7 @@ $forum_up = $_G['cache']['forums'][$_G['forum']['fup']];
 if($_G['forum']['type'] == 'forum') {
 	$fgroupid = $_G['forum']['fup'];
 	if(empty($_GET['archiveid'])) {
-		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?gid='.$forum_up['fid'].'">'.$forum_up['name'].'</a><em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$_G['forum']['fid'].'">'.$_G['forum']['name'].'</a>';
+		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$_G['forum']['fid'].'">'.$_G['forum']['name'].'</a>';
 	} else {
 		$navigation = ' <em>&rsaquo;</em> '.'<a href="forum.php?mod=forumdisplay&fid='.$_G['fid'].'">'.$_G['forum']['name'].'</a> <em>&rsaquo;</em> '.$forumarchive[$_GET['archiveid']]['displayname'];
 	}
@@ -853,6 +853,19 @@ if(!empty($_G['forum']['threadsorts']['templatelist']) && $_G['forum']['status']
 	write_groupviewed($_G['fid']);
 	$template = 'diy:group/group:'.$_G['fid'];
 }
+
+//add by kaiser
+$banzhuname = explode("\t", $_G['forum']['moderators']);
+$banzhulist = C::t('common_member')->fetch_all_by_username($banzhuname);
+//var_dump($banzhulist);
+foreach($banzhulist as $val){
+	$kaiser_username = $val['username'];
+	$kaiser_uid = $val['uid'];
+	$kaiser_avatar = avatar($kaiser_uid, 'middle');
+	//var_dump($kaiser_avatar);
+	$kaiser_banzhu .= '<a href="home.php?mod=space&uid='.$kaiser_uid.'" target="_blank">'.$kaiser_avatar.'<p class="caption">'.$kaiser_username.'</p></a>';
+}
+//end add
 
 if(!defined('IN_ARCHIVER')) {
 	include template($template);
