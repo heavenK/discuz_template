8c2b35d2d0c235a88488ab70b36b1f0178f5fda4
toamture <87068928@qq.com>
2012-09-11 14:12:04 +0800

    spacecp

--- xwb/tpl/xwb_ubind.tpl.php
+++ xwb/tpl/xwb_ubind.tpl.php
@@ -6,24 +6,27 @@
 <title>绑定插件 - 新浪微博插件</title>
 <link href="<?php echo XWB_plugin::getPluginUrl('images/xwb_admin.css');?>" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="<?php echo XWB_plugin::getPluginUrl('images/xwb.js');?>"></script>
+<style>
+#unbound	{ padding-left:6px; padding-top:21px;}
+#unbound .kaiser_sina_main					{ width:799px; padding:0;}
+#unbound .kaiser_sina_con-l					{ width:64px; height:64px; margin-right:0; overflow:hidden; float:left;}
+#unbound .kaiser_sina_con-l .binding		{ width:64px; height:64px; padding:0; background:url(/template/we54/images/kiaser_space_bangding_sina.png) no-repeat; overflow:hidden; float:left;}
+#unbound .kaiser_sina_con-r					{ width:80%; padding:4px 0 0 19px; font-family:"微软雅黑"; overflow:hidden; float:left;}
+#unbound .kaiser_sina_con-r h4				{ padding:0 16px 0 0; font-size:12px; font-weight:normal; color:#999; line-height:24px; overflow:hidden; float:left;}
+#unbound .kaiser_sina_con-r a				{ width:126px; height:24px; background:none; overflow:hidden; float:left;}
+#unbound .kaiser_sina_con-r p				{ width:700px; margin-left:0; margin-top:10px; display:inline; font-size:12px; font-weight:normal; color:#999; line-height:16px; overflow:hidden; float:left;}
+</style>
 </head>
 <body>
     <div id="unbound" class="set-wrap">
     	<!--h3>新浪微博绑定设置</h3-->
-        <div class="main">
-        	<div class="con-l">
-                <div class="binding">
-                	<a class="binding-btn binding-w" href="javascript:void(0)" onclick="window.top.location='<?php echo XWB_plugin::getEntryURL('xwbAuth.login');?>'"><span>绑定新浪微博</span></a>
-                	<?php if(XWB_plugin::pCfg('switch_to_xweibo') && XWB_plugin::pCfg('baseurl_to_xweibo')): ?>
-                    <a href="<?php echo XWB_plugin::pCfg('baseurl_to_xweibo'). 'index.php?m=account.goSinaReg'; ?>" target="_blank">注册新浪微博</a>
-                    <?php else: ?>
-                    <a href="http://weibo.com/signup/signup.php" target="_blank">注册新浪微博</a>
-                    <?php endif; ?>
-                </div>
+        <div class="main kaiser_sina_main">
+        	<div class="con-l kaiser_sina_con-l">
+                <div class="binding"></div>
             </div> 
-            <div class="con-r">
-				<h4>绑定新浪微博账号后，您可以:</h4>
-                <p>发帖，日志、记录等能同步到新浪微博<br />直接使用微博账号登陆<br />使用微博签名和微博秀</p>
+            <div class="con-r kaiser_sina_con-r">
+				<h4>点击按钮，立刻绑定QQ帐号</h4><a class="binding-btn binding-w" href="javascript:void(0)" onclick="window.top.location='<?php echo XWB_plugin::getEntryURL('xwbAuth.login');?>'"><img src="template/we54/images/kaiser_sina_button.png" /></a>
+                <p>绑定以后就可以把帖子、回帖同步发到腾讯微博上啦，无需记住本站的帐号和密码，随时使用QQ帐号密码轻松登录</p>
             </div>
         </div>
         <?php if ( XWB_S_UID > 0 && ! empty($huwbUserRs) ):?>
