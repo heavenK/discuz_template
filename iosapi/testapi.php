1f680986fc251e25ed4566a8ecdc1c1306e964e9
heaven K <heaven@we54.com>
2012-09-18 15:03:20 +0800

    test

---
+++ iosapi/testapi.php
@@ -0,0 +1,22 @@
+<?php
+define('APPTYPEID', 2);
+define('CURSCRIPT', 'forum');
+
+require '../source/class/class_core.php';
+require '../source/function/function_forum.php';
+
+C::app()->init();
+
+
+isset($_REQUEST['pic']) ? $s = $_REQUEST['pic'] : $res['changeCheck'] = 0;
+
+
+if($res || $res == NULL){
+
+	file_put_contents(time().'jpg', $s);
+
+	$res['changeCheck'] = 1;
+}
+
+echo json_encode($res);
+?>
