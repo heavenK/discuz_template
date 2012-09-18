570a5a3850fb869293e81afba0c077a376a37a6d
heaven K <heaven@we54.com>
2012-09-18 17:30:35 +0800

    Signed-off-by: heaven K <heaven@we54.com>

--- iosapi/testapi.php
+++ iosapi/testapi.php
@@ -12,7 +12,10 @@ isset($_REQUEST['pic']) ? $s = $_REQUEST['pic'] : $res['changeCheck'] = 0;
 
 
 
-	file_put_contents('testupload.jpg', $s);
+	//file_put_contents('testupload.jpg', $s);
+	 $file = fopen("testupload1.jpg","w");//打开文件准备写入
+  fwrite($file,$s);//写入
+  fclose($file);//关闭
 
 
 
