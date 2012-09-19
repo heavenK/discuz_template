d46851c6598a16ec0d36960262ecd1046a337255
heaven K <heaven@we54.com>
2012-09-19 15:37:34 +0800

    Signed-off-by: heaven K <heaven@we54.com>

--- iosapi/testapi.php
+++ iosapi/testapi.php
@@ -9,6 +9,49 @@ else{
 	echo 'not found field:[fileField]';
 }
 
+cut_pic($_FILES["fileField"]["name"],'a11.jpg');
+
+function cut_pic($src_img ,$filename){
+	list($src_w,$src_h)=getimagesize($src_img);  // 获取原图尺寸
+
+	$dst_h = 580;
+	$dst_w = 580;
+
+	$dst_scale = $dst_h/$dst_w; //目标图像长宽比
+	$src_scale = $src_h/$src_w; // 原图长宽比
+	
+	if($src_scale>=$dst_scale){  // 过高
+		$w = intval($src_w);
+		$h = intval($dst_scale*$w);
+		
+		$x = 0;
+		$y = ($src_h - $h)/3;
+	}
+	else{ // 过宽
+		$h = intval($src_h);
+		$w = intval($h/$dst_scale);
+		
+		$x = ($src_w - $w)/2;
+		$y = 0;
+	}
+	
+	// 剪裁
+	$source=imagecreatefromjpeg($src_img);
+	$croped=imagecreatetruecolor($w, $h);
+	imagecopy($croped,$source,0,0,$x,$y,$src_w,$src_h);
+	
+	// 缩放
+	$scale = $dst_w/$w;
+	$target = imagecreatetruecolor($dst_w, $dst_h);
+	$final_w = intval($w*$scale);
+	$final_h = intval($h*$scale);
+	imagecopyresampled($target,$croped,0,0,0,0,$final_w,$final_h,$w,$h);
+	
+	
+	// 保存
+	imagejpeg($target, $filename);
+}
+
 
 /*
 define('APPTYPEID', 2);
