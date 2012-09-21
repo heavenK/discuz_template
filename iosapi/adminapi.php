dd4c0f4442aaefbea30f695b1a8952f339b1951e
heaven K <heaven@we54.com>
2012-09-21 14:39:48 +0800

    Signed-off-by: heaven K <heaven@we54.com>

--- iosapi/adminapi.php
+++ iosapi/adminapi.php
@@ -20,46 +20,57 @@ if($_POST['type'] == 'dosubmit'){
 }
 
 ?>
+<style>
+
+.part1	{ float:left; width:1000px;}
+.part1 div:nth-child(2n+1)	{ float:left; width:500px;}
+.part1 div:nth-child(2n+1)	{ float:right; width:500px;}
+
+.part2	{ float:left; width:1000px;}
+.part2 div:nth-child(2n+1)	{ float:left; width:500px;}
+.part2 div:nth-child(2n+1)	{ float:right; width:500px;}
+
+</style>
 <form action="" method="post" enctype="multipart/form-data">
 <input type="hidden" name="type" value="dosubmit" />
 <h1>�޸ĵ�¼���ͼƬ</h1>
-<div>
-<div style="border:#03F thick;">
-<img src="welcome0.jpg" width="150" />
-ͼƬ1:<input type="file" name="pic0"  />
-</div>
-<div style="border:#03F thick;">
-<img src="welcome1.jpg" width="150" />
-ͼƬ2:<input type="file" name="pic1"  />
-</div>
-<div style="border:#03F thick;">
-<img src="welcome2.jpg" width="150" />
-ͼƬ3:<input type="file" name="pic2"  />
-</div>
-<div style="border:#03F thick;">
-<img src="welcome3.jpg" width="150" />
-ͼƬ4:<input type="file" name="pic3"  />
-</div>
+<div class="part1">
+    <div>
+        <img src="welcome0.jpg" width="150" />
+        ͼƬ1:<input type="file" name="pic0"  />
+    </div>
+    <div>
+        <img src="welcome1.jpg" width="150" />
+        ͼƬ2:<input type="file" name="pic1"  />
+    </div>
+    <div>
+        <img src="welcome2.jpg" width="150" />
+        ͼƬ3:<input type="file" name="pic2"  />
+    </div>
+    <div>
+        <img src="welcome3.jpg" width="150" />
+        ͼƬ4:<input type="file" name="pic3"  />
+    </div>
 </div>
 
 <h1>�޸Ĺ��</h1>
-<div>
-<div style="border:#03F thick;">
-<img src="ad00.jpg" width="150" />
-ͼƬ1:<input type="file" name="ad0"  />
-</div>
-<div style="border:#03F thick;">
-<img src="ad01.jpg" width="150" />
-ͼƬ2:<input type="file" name="ad1"  />
-</div>
-<div style="border:#03F thick;">
-<img src="ad02.jpg" width="150" />
-ͼƬ3:<input type="file" name="ad2"  />
-</div>
-<div style="border:#03F thick;">
-<img src="ad03.jpg" width="150" />
-ͼƬ4:<input type="file" name="ad3"  />
-</div>
+<div class="part2">
+    <div>
+        <img src="ad00.jpg" width="150" />
+        ͼƬ1:<input type="file" name="ad0"  />
+    </div>
+    <div>
+        <img src="ad01.jpg" width="150" />
+        ͼƬ2:<input type="file" name="ad1"  />
+    </div>
+    <div>
+        <img src="ad02.jpg" width="150" />
+        ͼƬ3:<input type="file" name="ad2"  />
+    </div>
+    <div>
+        <img src="ad03.jpg" width="150" />
+        ͼƬ4:<input type="file" name="ad3"  />
+    </div>
 </div>
 <input type="submit" value="submit" />
 </form>
