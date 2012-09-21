e924d3d7f8385864f4f8a0dd779af4760671a0ad
heaven K <heaven@we54.com>
2012-09-21 10:33:06 +0800

    Signed-off-by: heaven K <heaven@we54.com>

---
+++ iosapi/adminapi.php
@@ -0,0 +1,19 @@
+<?php
+define('APPTYPEID', 2);
+define('CURSCRIPT', 'forum');
+
+require '../source/class/class_core.php';
+require '../source/function/function_forum.php';
+
+?>
+<h1>修改登录随机图片</h1>
+<div>
+<img src="welcome0.jpg" />
+图片1:<input type="file" name="pic0"  /><br />
+<img src="welcome1.jpg" />
+图片2:<input type="file" name="pic0"  /><br />
+<img src="welcome2.jpg" />
+图片3:<input type="file" name="pic0"  /><br />
+<img src="welcome3.jpg" />
+图片4:<input type="file" name="pic0"  /><br />
+</div>
