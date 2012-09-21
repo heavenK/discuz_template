79dfe5c421c11c031fda84f40194a8022f630565
heaven K <heaven@we54.com>
2012-09-21 10:36:11 +0800

    Signed-off-by: heaven K <heaven@we54.com>

--- iosapi/adminapi.php
+++ iosapi/adminapi.php
@@ -8,12 +8,12 @@ require '../source/function/function_forum.php';
 ?>
 <h1>�޸ĵ�¼���ͼƬ</h1>
 <div>
-<img src="welcome0.jpg" />
+<img src="welcome0.jpg" width="200" />
 ͼƬ1:<input type="file" name="pic0"  /><br />
-<img src="welcome1.jpg" />
+<img src="welcome1.jpg" width="200" />
 ͼƬ2:<input type="file" name="pic0"  /><br />
-<img src="welcome2.jpg" />
+<img src="welcome2.jpg" width="200" />
 ͼƬ3:<input type="file" name="pic0"  /><br />
-<img src="welcome3.jpg" />
+<img src="welcome3.jpg" width="200" />
 ͼƬ4:<input type="file" name="pic0"  /><br />
 </div>
