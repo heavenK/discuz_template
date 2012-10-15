2a4cf0f8916a8d6f98c03fb723cfd8b5e6340e18
toamture <87068928@qq.com>
2012-10-15 13:24:55 +0800

    spacecp xwb and qq

--- xwb/tpl/xwb_ubind.tpl.php
+++ xwb/tpl/xwb_ubind.tpl.php
@@ -34,8 +34,18 @@ body{ background:#FFF;}
         </div>
         <?php endif;?>
         <?php if ( $_GET['ac'] == 'plugin' ):?>
+        	<div class="main kaiser_sina_main">
+                <div class="con-l kaiser_sina_con-l">
+                    <div class="binding"></div>
+                </div> 
+                <div class="con-r kaiser_sina_con-r">
+                    <h4>点击按钮，立刻绑定QQ帐号</h4>
+                    <a class="binding-btn binding-w" href="javascript:void(0)" onclick="window.top.location='<?php echo XWB_plugin::getEntryURL('xwbAuth.login');?>'"><img src="template/we54/images/kaiser_sina_bangding_button.png" /></a>
+                    <p>绑定以后就可以把帖子、回帖同步发到新浪微博上啦，无需记住本站的帐号和密码，随时使用新浪帐号密码轻松登录</p>
+                </div>
+            </div>
 			<?php if ( XWB_S_UID > 0 && ! empty($huwbUserRs) ):?>
-            <!--<div class="active-s1">
+            <div class="active-s1">
                 <h4>他们已经绑定微博了，你还不行动？</h4>
                 <?php foreach ($huwbUserRs as $value):?>
                 <div class="users">
@@ -47,7 +57,7 @@ body{ background:#FFF;}
                     </div>
                 </div>
                 <?php endforeach;?>
-            </div>-->
+            </div>
             <?php endif;?>
         <?php endif;?>
     </div>
