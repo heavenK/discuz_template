445176f45cd780f0826f25c2bcf63f9bd9cc586f
toamture <87068928@qq.com>
2012-10-15 10:16:53 +0800

    bangding

--- xwb/tpl/xwb_ubind.tpl.php
+++ xwb/tpl/xwb_ubind.tpl.php
@@ -21,29 +21,37 @@ body{ background:#FFF;}
 <body>
     <div id="unbound" class="set-wrap">
     	<!--h3>新浪微博绑定设置</h3-->
+        <?php if ( empty($_GET['id']) ):?>
         <div class="main kaiser_sina_main">
         	<div class="con-l kaiser_sina_con-l">
                 <div class="binding"></div>
             </div> 
             <div class="con-r kaiser_sina_con-r">
-				<h4>点击按钮，立刻绑定QQ帐号</h4><a class="binding-btn binding-w" href="javascript:void(0)" onclick="window.top.location='<?php echo XWB_plugin::getEntryURL('xwbAuth.login');?>'"><img src="template/we54/images/kaiser_sina_bangding_button.png" /></a>
+				<h4>点击按钮，立刻绑定QQ帐号</h4>
+                <?php if ( XWB_S_UID > 0 && ! empty($huwbUserRs) ):?>
+                <a class="binding-btn binding-w" href="http://dz.we54.com/home.php?mod=spacecp&ac=plugin&id=sina_xweibo_x2:home_binding">解除已绑定账号？</a>
+                <?php else:?>
+                <a class="binding-btn binding-w" href="javascript:void(0)" onclick="window.top.location='<?php echo XWB_plugin::getEntryURL('xwbAuth.login');?>'"><img src="template/we54/images/kaiser_sina_bangding_button.png" /></a>
+                <?php endif;?>
                 <p>绑定以后就可以把帖子、回帖同步发到新浪微博上啦，无需记住本站的帐号和密码，随时使用新浪帐号密码轻松登录</p>
             </div>
         </div>
-        <?php if ( XWB_S_UID > 0 && ! empty($huwbUserRs) ):?>
-        <div class="active-s1">
-        	<h4>他们已经绑定微博了，你还不行动？</h4>
-            <?php foreach ($huwbUserRs as $value):?>
-            <div class="users">
-                <a href="<?php echo XWB_plugin::getWeiboProfileLink($value['sina_uid']); ?>" target="_blank"><?php echo $value['avatar'];?></a>
-                <div class="user-info">
-                    <p><?php echo XWB_plugin::convertEncoding($value['username'], XWB_S_CHARSET, 'UTF-8');?></p>
-                    <a class="addfollow-btn" href="<?php echo XWB_plugin::getWeiboProfileLink($value['sina_uid']); ?>" target="_blank"></a>
-                    <a class="already-addfollow-btn hidden" href="javascript:void(0)#"></a>
+        <?php else:?>
+			<?php if ( XWB_S_UID > 0 && ! empty($huwbUserRs) ):?>
+            <div class="active-s1">
+                <h4>他们已经绑定微博了，你还不行动？</h4>
+                <?php foreach ($huwbUserRs as $value):?>
+                <div class="users">
+                    <a href="<?php echo XWB_plugin::getWeiboProfileLink($value['sina_uid']); ?>" target="_blank"><?php echo $value['avatar'];?></a>
+                    <div class="user-info">
+                        <p><?php echo XWB_plugin::convertEncoding($value['username'], XWB_S_CHARSET, 'UTF-8');?></p>
+                        <a class="addfollow-btn" href="<?php echo XWB_plugin::getWeiboProfileLink($value['sina_uid']); ?>" target="_blank"></a>
+                        <a class="already-addfollow-btn hidden" href="javascript:void(0)#"></a>
+                    </div>
                 </div>
+                <?php endforeach;?>
             </div>
-            <?php endforeach;?>
-        </div>
+            <?php endif;?>
         <?php endif;?>
     </div>
 </body>
